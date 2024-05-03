3aec36c439e0510e9b84fe0744841c4292de2c54
Stuti <kstuti88@gmail.com>
2024-05-04 00:48:21 +0530

    card update

---
+++ paneer.js
@@ -0,0 +1,29 @@
+const resultsList = document.getElementById('results');
+
+async function searchRecipes() {
+  const searchValue = "paneer"; // Always search for "paneer"
+  
+  const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=ff171625&app_key=ff010fa568178d2bd58190c6e5592112&from=0&to=40`);
+  const data = await response.json();
+  displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+  let html = '';
+  recipes.forEach((recipe) => {
+    html += `
+    <div>
+      <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+      <h3>${recipe.recipe.label}</h3>
+      <ul>
+        ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+      </ul>
+      <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+    </div>
+    `;
+  });
+  resultsList.innerHTML = html;
+}
+
+// Call the searchRecipes function when the page loads
+window.onload = searchRecipes;
